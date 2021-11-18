7128dfd6c839cd8a16a41fa0ab55b6818d7df6ee
Kyaw-Zin-Latt <kyawzinlat43021@gmail.com>
2021-11-18 13:10:46 +0630

    test

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_login_form()
+    {
+        $response = $this->get('login');
+
+        $response->assertStatus(200);
+    }
+}
